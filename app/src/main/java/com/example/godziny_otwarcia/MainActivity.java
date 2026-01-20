34de14cb074f6e67fc40bf47f6b63681964c9272
LordOwing <michallolo12345@gmail.com>
2026-01-20 20:17:21 +0100

    Initial commit

---
+++ app/src/main/java/com/example/godziny_otwarcia/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.godziny_otwarcia;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TimePicker;
+import android.widget.Toast;
+import java.time.LocalTime;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TimePicker timePicker;
+    private Button btnCheck;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        timePicker = findViewById(R.id.timePicker);
+        btnCheck = findViewById(R.id.btnCheck);
+
+
+        timePicker.setIs24HourView(true);
+
+        btnCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkOpeningHours();
+            }
+        });
+    }
+
+    private void checkOpeningHours() {
+
+        int hour = timePicker.getHour();
+        int minute = timePicker.getMinute();
+
+
+        LocalTime selectedTime = LocalTime.of(hour, minute);
+        LocalTime openingTime = LocalTime.of(8, 0);    // 08:00
+        LocalTime closingTime = LocalTime.of(16, 0);   // 16:00
+
+
+        boolean isOpen = !selectedTime.isBefore(openingTime) && selectedTime.isBefore(closingTime);
+
+
+        if (isOpen) {
+            Toast.makeText(this, "Otwarte, zapraszamy!", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "Zamknięte", Toast.LENGTH_LONG).show();
+        }
+    }
+}
